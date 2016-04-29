1984a41932cc97bf70f8399fc22564a6c1e8f28f
Wladimir Wendland <wladimir.wendland@gmail.com>
2016-04-29 14:46:07 +0200

    Use HttpKernel Component: the Controller Resolver

--- web/front.php
+++ web/front.php
@@ -5,8 +5,9 @@ require_once __DIR__.'/../vendor/autoload.php';
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing;
+use Symfony\Component\HttpKernel;
 
-function render_template($request)
+function render_template(Request $request)
 {
     extract($request->attributes->all(), EXTR_SKIP);
     ob_start();
@@ -21,10 +22,15 @@ $routes = include __DIR__.'/../src/app.php';
 $context = new Routing\RequestContext();
 $context->fromRequest($request);
 $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
+$resolver = new HttpKernel\Controller\ControllerResolver();
 
 try {
     $request->attributes->add($matcher->match($request->getPathInfo()));
-    $response = call_user_func($request->attributes->get('_controller'), $request);
+
+    $controller = $resolver->getController($request);
+    $arguments = $resolver->getArguments($request, $controller);
+
+    $response = call_user_func_array($controller, $arguments);
 } catch (Routing\Exception\ResourceNotFoundException $e) {
     $response = new Response('Not Found', 404);
 } catch (Exception $e) {
