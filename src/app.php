2ed06089fe215bbaae8bfac6144fcfa68633386f
Wladimir Wendland <wladimir.wendland@gmail.com>
2016-04-29 14:50:22 +0200

    The Separation of Concerns

--- src/app.php
+++ src/app.php
@@ -1,25 +1,12 @@
 <?php
 
 use Symfony\Component\Routing;
-use Symfony\Component\HttpFoundation\Response;
-
-class LeapYearController
-{
-    public function indexAction($request)
-    {
-        if (is_leap_year($request->attributes->get('year'))) {
-            return new Response('Yep, this is a leap year!');
-        }
-
-        return new Response('Nope, this is not a leap year.');
-    }
-}
 
 $routes = new Routing\RouteCollection();
 
 $routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', array(
     'year' => null,
-    '_controller' => array(new LeapYearController(), 'indexAction'),
+    '_controller' => 'Calendar\\Controller\\LeapYearController::indexAction',
 )));
 
 return $routes;
