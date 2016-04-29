ef8e24f9c48015e3369f9a601bddbc9da8447332
Wladimir Wendland <wladimir.wendland@gmail.com>
2016-04-29 14:30:33 +0200

    Sample for Routing Generator

--- src/app.php
+++ src/app.php
@@ -7,3 +7,37 @@ $routes->add('hello', new Routing\Route('/hello/{name}', array('name' => 'World'
 $routes->add('bye', new Routing\Route('/bye'));
 
 return $routes;
+
+
+/*
+
+use Symfony\Component\Routing;
+
+$generator = new Routing\Generator\UrlGenerator($routes, $context);
+
+echo $generator->generate('hello', array('name' => 'Fabien'));
+// outputs /hello/Fabien
+
+*/
+
+
+/*
+
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+echo $generator->generate(
+    'hello',
+    array('name' => 'Fabien'),
+    UrlGeneratorInterface::ABSOLUTE_URL
+);
+// outputs something like http://example.com/somewhere/hello/Fabien
+
+*/
+
+/*
+
+$dumper = new Routing\Matcher\Dumper\PhpMatcherDumper($routes);
+
+echo $dumper->dump();
+
+ */
