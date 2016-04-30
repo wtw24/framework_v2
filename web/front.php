a1d0c23b13f5b0bc58fafc59971f9b07e510ad90
Wladimir Wendland <wladimir.wendland@gmail.com>
2016-04-30 10:17:28 +0200

    Add ResponseEvent

--- web/front.php
+++ web/front.php
@@ -5,6 +5,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing;
 use Symfony\Component\HttpKernel;
+use Symfony\Component\EventDispatcher\EventDispatcher;
 
 $request = Request::createFromGlobals();
 $routes = include __DIR__.'/../src/app.php';
@@ -13,7 +14,21 @@ $context = new Routing\RequestContext();
 $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 $resolver = new HttpKernel\Controller\ControllerResolver();
 
-$framework = new Simplex\Framework($matcher, $resolver);
+$dispatcher = new EventDispatcher();
+$dispatcher->addListener('response', function (Simplex\ResponseEvent $event) {
+    $response = $event->getResponse();
+
+    if ($response->isRedirection()
+        || ($response->headers->has('Content-Type') && false === strpos($response->headers->get('Content-Type'), 'html'))
+        || 'html' !== $event->getRequest()->getRequestFormat()
+    ) {
+        return;
+    }
+
+    $response->setContent($response->getContent().'GA CODE');
+});
+
+$framework = new Simplex\Framework($dispatcher, $matcher, $resolver);
 $response = $framework->handle($request);
 
 $response->send();
