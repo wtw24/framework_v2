922815952e3cd4b665864aa13f11fba6d6f67999
Wladimir Wendland <wladimir.wendland@gmail.com>
2016-04-30 10:54:41 +0200

    Register a Custom Listener

--- web/front.php
+++ web/front.php
@@ -7,6 +7,11 @@ use Symfony\Component\HttpFoundation\Request;
 $routes = include __DIR__.'/../src/app.php';
 $sc = include __DIR__.'/../src/container.php';
 
+$sc->register('listener.string_response', 'Simplex\StringResponseListener');
+$sc->getDefinition('dispatcher')
+    ->addMethodCall('addSubscriber', array(new Reference('listener.string_response')))
+;
+
 $request = Request::createFromGlobals();
 
 $response = $sc->get('framework')->handle($request);
