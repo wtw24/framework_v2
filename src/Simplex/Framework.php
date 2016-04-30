5a62c82f219fb6ca5280d9ab97e4dbc260aef286
Wladimir Wendland <wladimir.wendland@gmail.com>
2016-04-30 10:50:38 +0200

    Code Refactoring

--- src/Simplex/Framework.php
+++ src/Simplex/Framework.php
@@ -2,8 +2,22 @@
 
 namespace Simplex;
 
-use Symfony\Component\HttpKernel\HttpKernel;
+use Symfony\Component\Routing;
+use Symfony\Component\HttpKernel;
+use Symfony\Component\EventDispatcher\EventDispatcher;
 
-class Framework extends HttpKernel
+class Framework extends HttpKernel\HttpKernel
 {
+    public function __construct($routes)
+    {
+        $context = new Routing\RequestContext();
+        $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
+        $resolver = new HttpKernel\Controller\ControllerResolver();
+
+        $dispatcher = new EventDispatcher();
+        $dispatcher->addSubscriber(new HttpKernel\EventListener\RouterListener($matcher));
+        $dispatcher->addSubscriber(new HttpKernel\EventListener\ResponseListener('UTF-8'));
+
+        parent::__construct($dispatcher, $resolver);
+    }
 }
--- web/front.php
+++ web/front.php
@@ -3,32 +3,10 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing;
-use Symfony\Component\HttpKernel;
-use Symfony\Component\EventDispatcher\EventDispatcher;
-use Symfony\Component\HttpFoundation\RequestStack;
 
 $request = Request::createFromGlobals();
 $routes = include __DIR__.'/../src/app.php';
 
-$context = new Routing\RequestContext();
-$matcher = new Routing\Matcher\UrlMatcher($routes, $context);
-$resolver = new HttpKernel\Controller\ControllerResolver();
+$framework = new Simplex\Framework($routes);
 
-$dispatcher = new EventDispatcher();
-$dispatcher->addSubscriber(new HttpKernel\EventListener\RouterListener($matcher, new RequestStack()));
-
-$listener = new HttpKernel\EventListener\ExceptionListener(
-    'Calendar\\Controller\\ErrorController::exceptionAction'
-);
-$dispatcher->addSubscriber($listener);
-
-$dispatcher->addSubscriber(new HttpKernel\EventListener\ResponseListener('UTF-8'));
-$dispatcher->addSubscriber(new HttpKernel\EventListener\StreamedResponseListener());
-$dispatcher->addSubscriber(new Simplex\StringResponseListener());
-
-$framework = new Simplex\Framework($dispatcher, $resolver);
-
-$response = $framework->handle($request);
-$response->send();
+$framework->handle($request)->send();
