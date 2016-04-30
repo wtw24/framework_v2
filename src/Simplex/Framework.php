eec88fa080c8a8d092c0251726b96bc3112473a3
Wladimir Wendland <wladimir.wendland@gmail.com>
2016-04-30 10:53:32 +0200

    Use Symfony Dependency Injection Container

--- src/Simplex/Framework.php
+++ src/Simplex/Framework.php
@@ -2,22 +2,8 @@
 
 namespace Simplex;
 
-use Symfony\Component\Routing;
-use Symfony\Component\HttpKernel;
-use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Component\HttpKernel\HttpKernel;
 
-class Framework extends HttpKernel\HttpKernel
+class Framework extends HttpKernel
 {
-    public function __construct($routes)
-    {
-        $context = new Routing\RequestContext();
-        $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
-        $resolver = new HttpKernel\Controller\ControllerResolver();
-
-        $dispatcher = new EventDispatcher();
-        $dispatcher->addSubscriber(new HttpKernel\EventListener\RouterListener($matcher));
-        $dispatcher->addSubscriber(new HttpKernel\EventListener\ResponseListener('UTF-8'));
-
-        parent::__construct($dispatcher, $resolver);
-    }
 }
--- web/front.php
+++ web/front.php
@@ -4,9 +4,11 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
 
-$request = Request::createFromGlobals();
 $routes = include __DIR__.'/../src/app.php';
+$sc = include __DIR__.'/../src/container.php';
 
-$framework = new Simplex\Framework($routes);
+$request = Request::createFromGlobals();
 
-$framework->handle($request)->send();
+$response = $sc->get('framework')->handle($request);
+
+$response->send();
